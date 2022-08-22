1721b1af1cca824405d6c4469b3844529b3f91c7
Kevin <kevin.haya@yahoo.com>
2022-08-22 22:19:56 +0800

    Refractor script.js

---
+++ README.md
@@ -0,0 +1,6 @@
+Project: Tic Tac Toe from The Odin project:
+    https://www.theodinproject.com/lessons/node-path-javascript-tic-tac-toe
+
+Project guided by:
+    https://www.webtips.dev/tic-tac-toe-in-javascript
+    by:https://github.com/dcode-youtube
--- script.js
+++ script.js
@@ -1,69 +1,14 @@
-// const game = {
-//     xTurn: true,
-//     xState: [],
-//     oState: [],
-//     winningStates: [
-//         // Rows
-//         ['0', '1', '2'],
-//         ['3', '4', '5'],
-//         ['6', '7', '8'],
+const game = (()=>{
+    const human = {
+        mark:"x",
+        state:[]
+    }
+    const computer = {
+        mark:"o",
+        state:[]
+    }
 
-//         // Columns
-//         ['0', '3', '6'],
-//         ['1', '4', '7'],
-//         ['2', '5', '8'],
-
-//         // Diagonal
-//         ['0', '4', '8'],
-//         ['2', '4', '6']
-//     ]
-// }
-
-// document.addEventListener('click', event => {
-//     console.log("clicked")
-//     const target = event.target
-//     const isField = target.classList.contains('field')
-//     const isDisabled = target.classList.contains('disabled')
-
-//     if (isField && !isDisabled) {
-//         // The player clicked on a cell that is still empty
-//         const cellValue = target.dataset.value
-
-//         game.xTurn === true
-//             ? game.xState.push(cellValue)
-//             : game.oState.push(cellValue)
-
-//         target.classList.add('disabled')
-//         target.classList.add(game.xTurn ? 'x' : 'o')
-
-//         game.xTurn = !game.xTurn
-//     }
-//     if (!document.querySelectorAll('.field:not(.disabled)').length) {
-//         // document.querySelector('.game-over').classList.add('visible')
-//         // document.querySelector('.game-over-text').textContent = 'Draw!'
-//         alert("DRAW")
-//     }
-//     game.winningStates.forEach(winningState => {
-//         const xWins = winningState.every(state => game.xState.includes(state))
-//         const oWins = winningState.every(state => game.oState.includes(state))
-       
-//         if (xWins || oWins) {
-//             document.querySelectorAll('.field').forEach(cell => cell.classList.add('disabled'))
-//             // document.querySelector('.game-over').classList.add('visible')
-//             // document.querySelector('.game-over-text').textContent = xWins
-//             //     ? 'X wins!'
-//             //     : 'O wins!'
-            
-//         }
-//         if(xWins){
-//             alert("xwins")
-//         }
-//         if(oWins){
-//             alert("owins")
-//         }
-//     })
-// })
-const compare = (()=>{
+const compareState = (()=>{
     winningStates = [
       // Rows
       ['0', '1', '2'],
@@ -77,61 +22,106 @@ const compare = (()=>{
       ['0', '4', '8'],
       ['2', '4', '6']
     ];
-    const updatedArr = () =>{
-      const modal = document.querySelector('.modal')
-      const result = document.querySelector(".result")
-      winningStates.forEach(winningState => {
-        const humanWin = winningState.every(state => human.state.includes(state))
-        const computerWin = winningState.every(state => computer.state.includes(state))
-        if(humanWin){
-          console.log("human win: "+human.state)
-        }
-        if(computerWin){
-          console.log("computer win"+computer.state)
-        }
-        if (!document.querySelectorAll('.field:not(.disabled)').length){        
-          modal.style.display = "flex";
-          result.textContent = "DRAW!"
-        }
+    const isThereAWinner = () =>{
+        winningStates.forEach(winningState => {
+            const humanWin = winningState.every(state => human.state.includes(state))
+            const computerWin = winningState.every(state => computer.state.includes(state))
+            if(humanWin){
+              displayController.humanWin();
+            }
+            if(computerWin){
+              displayController.computerWin();
+            }
+            if(!document.querySelectorAll('.field:not(.disabled)').length){        
+                displayController.draw();
+            }
       })
     }
     return{
-      updatedArr
+        isThereAWinner
     }
-  })()
-  
-  // best practice event delegation?
-  const board = document.getElementsByClassName("board")[0]
-  board.addEventListener('click', event => {
-    console.log("clicked")
-    const target = event.target
-    const field = target.classList.contains('field')
-    const disabled = target.classList.contains('disabled')
-    if (field && !disabled) {
-      // The player clicked on a cell that is still empty
-      const fieldValue = target.dataset.value
-      human.state.push(fieldValue)
-      target.classList.add('disabled')
-      target.classList.add(human.mark ? 'x' : 'o')
-  
-      // computerMove();
+})()
+
+const displayController = (()=>{
+    // from inputController()
+    const markBoard = (target) =>{
+        const disabled = target.classList.contains('disabled')
+        if(!disabled){
+            human.state.push(target.dataset.value)
+            target.classList.add('disabled')
+            target.classList.add(human.mark ? 'x' : 'o')
+            // computerMove(); // TODO
+        }
     }
-  
-    compare.updatedArr();
-  })
-  
-  function makeDefault(){
-    human.mark = "x";
-    human.state = [];
-    computer.mark = "o";
-    computer.state = [];
-  }
-  
-  const human = {
-    mark:"x",
-    state:[]
-  }
-  const computer = {
-    mark:"o",
-    state:[]
-  }
+    const xMark = (target) =>{
+        const active = target.classList.contains('active')
+        if(!active){
+            const oMark = document.getElementById("o-choice")
+            oMark.classList.remove("active")
+            target.classList.add("active")
+            human.mark = "x"
+        }
+    }
+    const oMark = (target) =>{
+        const active = target.classList.contains('active')
+        if(!active){
+            const xMark = document.getElementById("x-choice")
+            xMark.classList.remove("active")
+            target.classList.add("active")
+            human.mark = "o"
+        }
+    }
+    const restart = () =>{
+        document.querySelectorAll(".field").forEach(field => {
+            field.classList.remove('disabled', 'x', 'o')
+        })
+    }
+    // from compareState.isThereAWinner()
+    const humanWin = () =>{
+        console.log("human win: "+human.state)
+    }
+    const computerWin = () =>{
+        console.log("computer win: "+computer.state)
+    }
+    const draw = () =>{
+        const modal = document.querySelector('.modal')// go higher scope when needed
+        const result = document.querySelector(".result")// go higher scope when needed
+        modal.style.display = "flex";
+        result.textContent = "DRAW!"
+    }
+    return {
+        markBoard,
+        xMark,
+        oMark,
+        restart,
+        humanWin,
+        computerWin,
+        draw
+    }
+})()
+
+const inputController = (()=>{
+    const field = document.getElementsByClassName("board")[0]
+    const xMark = document.getElementById("x-choice")
+    const oMark = document.getElementById("o-choice")
+    const restart = document.getElementById("restart")
+
+    field.addEventListener('click', event => {
+        displayController.markBoard(event.target)
+        compareState.isThereAWinner();
+    })
+    xMark.addEventListener("click",event => {
+        displayController.xMark(event.target)
+    })
+    oMark.addEventListener("click",event => {
+        displayController.oMark(event.target)
+    })
+    restart.addEventListener("click",event => {
+        displayController.restart()
+        human.state = [];
+        computer.state = [];
+    })
+})()
+
+})()
+
